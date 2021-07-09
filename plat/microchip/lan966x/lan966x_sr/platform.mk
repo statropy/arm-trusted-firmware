a0ae362feec7e95e93c8e105b2a40103ecbc6e87
Lars Povlsen <lars.povlsen@microchip.com>
2021-07-09 15:45:36 +0200

    lan966x: otp: TEMPORARY disable of LAN966X_TZ

--- plat/microchip/lan966x/lan966x_sr/platform.mk
+++ plat/microchip/lan966x/lan966x_sr/platform.mk
@@ -2,4 +2,5 @@ CONSOLE_BASE			:=	LAN966X_FLEXCOM_0_BASE
 
 include plat/microchip/lan966x/common/common.mk
 
-$(eval $(call add_define,LAN966X_TZ))
+# Disabled due to problem popping up again
+# $(eval $(call add_define,LAN966X_TZ))
