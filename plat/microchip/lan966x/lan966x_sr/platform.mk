e147b3799b4ddb096248e4a1d99c4d0d4e88522f
Lars Povlsen <lars.povlsen@microchip.com>
2021-07-19 13:01:51 +0200

    lan966x: tz: Honour otp_flags1_enable_ddr_encrypt, revert to fixed keys due to FPGA issue (no TRNG)

--- plat/microchip/lan966x/lan966x_sr/platform.mk
+++ plat/microchip/lan966x/lan966x_sr/platform.mk
@@ -2,5 +2,4 @@ CONSOLE_BASE			:=	LAN966X_FLEXCOM_0_BASE
 
 include plat/microchip/lan966x/common/common.mk
 
-# Disabled due to problem popping up again
-# $(eval $(call add_define,LAN966X_TZ))
+$(eval $(call add_define,LAN966X_TZ))
