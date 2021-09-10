606f0a340e4f7f20fdec19a8897d3bb50578fed7
Lars Povlsen <lars.povlsen@microchip.com>
2021-09-10 10:17:16 +0200

    lan966x: Adjust BL1 SRAM usage for mbedTLS

--- plat/microchip/lan966x/common/include/platform_def.h
+++ plat/microchip/lan966x/common/include/platform_def.h
@@ -52,7 +52,7 @@
  * the current BL1 RW debug size plus a little space for growth.
  */
 #define BL1_RW_BASE		(BL1_RW_LIMIT - BL1_RW_SIZE)
-#define BL1_RW_SIZE		SIZE_K(20)
+#define BL1_RW_SIZE		SIZE_K(36)
 #define BL1_RW_LIMIT		(LAN996X_SRAM_BASE + LAN996X_SRAM_SIZE)
 #endif
 
