8b70d11be30f4f4d85f9d6765ad90e69395843fe
Lars Povlsen <lars.povlsen@microchip.com>
2021-10-06 13:59:35 +0200

    lan966x: fw_config: Make code align better with documentation

--- plat/microchip/lan966x/common/lan966x_common.c
+++ plat/microchip/lan966x/common/lan966x_common.c
@@ -408,35 +408,38 @@ int lan966x_load_fw_config_raw(unsigned int image_id)
 
 int lan966x_load_fw_config(unsigned int image_id)
 {
-	uint8_t config[FW_CONFIG_MAX_DATA];
-	int result;
+	int result = 0;
 
-	/* Save the current config */
-	memcpy(config, lan966x_fw_config.config, sizeof(config));
+	/* If ROTPK not set, read fw_config */
+	if (otp_emu_init()) {
+		uint8_t config[FW_CONFIG_MAX_DATA];
 
-	/* The FW_CONFIG is first read *wo* authentication. */
-	result = lan966x_load_fw_config_raw(image_id);
+		/* Save the current config */
+		memcpy(config, lan966x_fw_config.config, sizeof(config));
 
-	/* If OK, then *authenticate* */
-	if (result == 0) {
-		otp_emu_init();
+		/* The FW_CONFIG is first read *wo* authentication. */
+		result = lan966x_load_fw_config_raw(image_id);
+
+		/* If OK, then *authenticate* */
+		if (result == 0) {
 #ifdef IMAGE_BL1		/* Only authenticate at BL1 */
-		image_desc_t *desc = bl1_plat_get_image_desc(image_id);
+			image_desc_t *desc = bl1_plat_get_image_desc(image_id);
 
-		if (desc == NULL) {
-			result = -ENOENT;
-		} else {
-			result = load_auth_image(image_id, &desc->image_info);
-			if (result != 0)
-				ERROR("FW_CONFIG did not authenticate: rc %d\n", result);
-		}
+			if (desc == NULL) {
+				result = -ENOENT;
+			} else {
+				result = load_auth_image(image_id, &desc->image_info);
+				if (result != 0)
+					ERROR("FW_CONFIG did not authenticate: rc %d\n", result);
+			}
 #endif
-	}
+		}
 
-	/* If something went wrong, restore fw_config */
-	if (result != 0) {
-		memset(&lan966x_fw_config, 0, sizeof(lan966x_fw_config));
-		memcpy(lan966x_fw_config.config, config, sizeof(config));
+		/* If something went wrong, restore fw_config */
+		if (result != 0) {
+			memset(&lan966x_fw_config, 0, sizeof(lan966x_fw_config));
+			memcpy(lan966x_fw_config.config, config, sizeof(config));
+		}
 	}
 
 	return result;
