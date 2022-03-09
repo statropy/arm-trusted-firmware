125605ca561321c75edf6d791015d35ee717a4dc
Lars Povlsen <lars.povlsen@microchip.com>
2022-03-09 12:33:49 +0100

    lan966x: Expose NS devices for Linux

--- plat/microchip/lan966x/common/sp_min/sp_min_plat_setup.c
+++ plat/microchip/lan966x/common/sp_min/sp_min_plat_setup.c
@@ -155,6 +155,17 @@ static void configure_sram(void)
 					MATRIX_SRTOP(0, MATRIX_SRTOP_VALUE_128K),
 					MATRIX_SASPLIT(0, MATRIX_SRTOP_VALUE_128K),
 					MATRIX_LANSECH_NS(0));
+	/* Enable USB for NS access.
+	 * Has two RAMs, MAP:128K + xHCI:512K
+	 */
+	matrix_configure_slave_security(MATRIX_SLAVE_USB,
+					MATRIX_SRTOP(1, MATRIX_SRTOP_VALUE_512K) |
+					MATRIX_SRTOP(0, MATRIX_SRTOP_VALUE_128K),
+					MATRIX_SASPLIT(1, MATRIX_SRTOP_VALUE_512K) |
+					MATRIX_SASPLIT(0, MATRIX_SRTOP_VALUE_128K),
+					MATRIX_RDNSECH(0, 1) | MATRIX_RDNSECH(1, 1) |
+					MATRIX_WRNSECH(0, 1) | MATRIX_WRNSECH(1, 1));
+
 }
 
 void sp_min_plat_runtime_setup(void)
