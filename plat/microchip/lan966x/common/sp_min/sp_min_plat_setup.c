a141c796c2f6dc9fb6d7da25221dca57edc7bee8
Lars Povlsen <lars.povlsen@microchip.com>
2022-05-16 11:00:48 +0200

    lan966x: tz: Revert SRAM DMAC flexram1 NS access (UNG_MASERATI-939)

--- plat/microchip/lan966x/common/sp_min/sp_min_plat_setup.c
+++ plat/microchip/lan966x/common/sp_min/sp_min_plat_setup.c
@@ -150,6 +150,12 @@ static void configure_sram(void)
 					MATRIX_SRTOP(0, MATRIX_SRTOP_VALUE_128K),
 					MATRIX_SASPLIT(0, MATRIX_SRTOP_VALUE_128K),
 					MATRIX_LANSECH_NS(0));
+	/* - and DMAC SRAM access */
+	matrix_configure_slave_security(MATRIX_SLAVE_FLEXRAM1,
+					MATRIX_SRTOP(0, MATRIX_SRTOP_VALUE_128K),
+					MATRIX_SASPLIT(0, MATRIX_SRTOP_VALUE_128K),
+					MATRIX_LANSECH_NS(0));
+
 	/* Enable QSPI0 for NS access */
 	matrix_configure_slave_security(MATRIX_SLAVE_QSPI0,
 					MATRIX_SRTOP(0, MATRIX_SRTOP_VALUE_16M) |
