3b9ac145834022408ebd282b1d72309f0809254e
Lars Povlsen <lars.povlsen@microchip.com>
2022-06-07 15:29:30 +0200

    lan966x: psci: SHA not required for bind after all (TFA-99)

--- plat/microchip/lan966x/common/lan966x_tz.c
+++ plat/microchip/lan966x/common/lan966x_tz.c
@@ -33,6 +33,7 @@ static void setup_ns_access(uintptr_t gpv, uintptr_t tzpm)
 			TZPM_TZPCTL1_FLEXCOM2(1) |
 			TZPM_TZPCTL1_FLEXCOM3(1) |
 			TZPM_TZPCTL1_FLEXCOM4(1) |
+			TZPM_TZPCTL1_SHA(1) |
 			TZPM_TZPCTL1_XDMA(1) |
 			TZPM_TZPCTL1_QSPI1(1) |
 			TZPM_TZPCTL1_TRNG(1));
