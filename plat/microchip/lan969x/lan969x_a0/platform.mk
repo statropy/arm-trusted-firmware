1f971c5b0f4a06fe5538c1d0d08c8da3f02274f6
Lars Povlsen <lars.povlsen@microchip.com>
2022-09-05 14:36:38 +0200

    lan969x: clk: Fix clock driver defines, add new clock ids (UNG_LAGUNA-371)

--- plat/microchip/lan969x/lan969x_a0/platform.mk
+++ plat/microchip/lan969x/lan969x_a0/platform.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
+include plat/microchip/lan969x/common/common.mk
+
 # This is used in lan969x code
 $(eval $(call add_define,LAN969X_ASIC))
 # This is used in common drivers
 $(eval $(call add_define,LAN966X_ASIC))
-
-include plat/microchip/lan969x/common/common.mk
